e4704818ff98c480198559158784aa2fa5a210cc
KVCareers <kylevarnercareers@gmail.com>
2024-10-30 22:55:38 -0500

    first commit

---
+++ src/business_logic/hello-world.service.ts
@@ -0,0 +1,6 @@
+
+export class HelloWorldService {
+    helloWorld(): void {
+        console.log('Hello World!');
+    }
+}
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+
+// Application Entry Point
+import { HelloWorldService } from './business_logic/hello-world.service.js';
+
+const helloWorldService = new HelloWorldService();
+helloWorldService.helloWorld();
