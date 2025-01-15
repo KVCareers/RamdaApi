c95963e394127d47edbf3afd0d7d879b8bba3531
KVCareers <kylevarnercareers@gmail.com>
2025-01-15 00:55:30 -0600

    adjust api rate limiting rate

--- src/business_logic/services/api_rate_limit_delay_service/api-rate-limit-delay.service.ts
+++ src/business_logic/services/api_rate_limit_delay_service/api-rate-limit-delay.service.ts
@@ -1,6 +1,6 @@
 
 export class ApiRateLimitDelayService {
-    readonly requestDelayTime = 500;
+    readonly requestDelayTime = 200;
     
     async delayRequest(): Promise<void> {
         console.log(`Delaying the next request by ${this.requestDelayTime} milliseconds to avoid rate limiting.`);
